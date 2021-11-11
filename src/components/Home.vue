b8a3eb315e287a0d3b9ea6db734f83366bc20aa9
Chris Philbin <chris@chrisphilbin.net>
2021-11-11 16:05:49 -0500

    Adding nav bar

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-center">
+    <h2>Under Construction</h2>
+    <img
+      src="../assets/images/logo/coastal_coating_logo_large.jpg"
+      class="mx-auto"
+    />
+  </div>
+</template>
+
+<script></script>
---
+++ src/components/navigation/TopNav.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="relative bg-black w-full h-auto">
+    <img
+      src="../../assets/images/logo/coastal_coating_logo_small_transparent.png"
+      class="transform scale-75"
+    />
+
+    <div class="md:hidden absolute right-4 top-5 flex items-center">
+      <button class="outline-none mobile-menu-button">
+        <svg
+          class="w-10 h-10 text-white"
+          x-show="!showMenu"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopNav",
+};
+</script>
