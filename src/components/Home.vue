762b9ba8ae3198ba55825000620a95532e822f46
Chris Philbin <chris@chrisphilbin.net>
2021-11-14 22:26:42 -0500

    Addressing mobile styling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,9 @@
     <div class="relative md:hidden w-full">
       <img src="/img/home/corvette_mobile.jpg" class="h-auto w-full opacity-75" />
       <div class="absolute w-full mr-auto ml-auto bottom-1/2 text-3xl text-white font-sans z-10">We've got you covered.</div>
-      <button class="absolute left-20 bottom-6 h-16 w-52 rounded-lg bg-blue-400 text-white font-semibold text-lg z-10">Book appointment</button>
+      <!-- <button class="absolute left-20 bottom-6 h-16 w-52 rounded-lg bg-blue-400 text-white font-semibold text-lg z-10">Book appointment</button> -->
+      <button class="absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-5 h-16 w-52 rounded-lg bg-blue-400 text-white font-semibold text-lg z-10">Book appointment</button>
+
     </div>
 
     <div class="grid md:grid-cols-2 grid-cols-1 gap-4 bg-black text-white py-8">
