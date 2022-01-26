a2ac23c1af25a7e35b525c3f8b0dd6610f51d79f
Chris Philbin <chris@chrisphilbin.net>
2022-01-26 07:42:06 -0500

    Updating landing page

--- src/components/landing/LandingPage.vue
+++ src/components/landing/LandingPage.vue
@@ -1,27 +1,28 @@
 <template>
-  <div class="grid grid-cols-1">
+  <div class="grid grid-cols-1 mt-3">
     <div class="relative">
       <img
-        src="/img/services/ceramic-coating/ceramic-coating-main.jpg"
-        class="w-full h-auto ml-auto mr-auto mb-8 opacity-75"
+        src="/img/landing/corvettes.jpg"
+        class="w-full h-auto ml-auto mr-auto mb-8 opacity-80"
       />
       <p
         class="
           absolute
+          text-center
+          px-16
+          w-full
           top-1/2
-          left-32
-          md:left-1/2
+          left-1/2
           transform
-          -translate-y-1/2
-          md:-translate-x-1/2
-          text-2xl
+          -translate-y-1/2 -translate-x-1/2
+          text-3xl
           md:text-6xl
           text-white
           font-coastal-body font-bold
           z-10
         "
       >
-        The Grand Strand's preferred ceramic coating professional.
+        We are the Grand Strand's preferred ceramic coating professional.
       </p>
     </div>
   </div>
