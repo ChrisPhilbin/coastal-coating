4eec95992399e8e6aeb1607b3efdb688e9019fed
Chris Philbin <chrisp@runway.com.au>
2022-11-18 06:34:52 -0500

    Adjusting size of array so there are 8 pairs of cards and removing unused code

--- src/components/kiosk/Kiosk.vue
+++ src/components/kiosk/Kiosk.vue
@@ -3,11 +3,7 @@
 </style>
 
 <template>
-  <div
-    class="grid grid-cols-6 gap-8 m-4"
-    :class="{ 'pointer-events-none': isModalVisible }"
-    id="cardGame"
-  >
+  <div class="grid grid-cols-6 gap-8 m-4" :class="{ 'pointer-events-none': isModalVisible }" id="cardGame">
     <div
       v-for="(card, index) in memoryCards"
       class="text-white flip-container my-auto"
@@ -16,10 +12,7 @@
     >
       <div class="memorycard">
         <div class="border rounded shadow p-4" v-if="!card.isFlipped">
-          <img
-            src="/img/kiosk/cc_logo_small_transparent.png"
-            class="origin-center rotate-45"
-          />
+          <img src="/img/kiosk/cc_logo_small_transparent.png" class="origin-center rotate-45" />
         </div>
         <div class="rounded border" v-else><img :src="card.image" /></div>
       </div>
@@ -45,11 +38,12 @@
 import _cloneDeep from "lodash/cloneDeep";
 import _shuffle from "lodash/shuffle";
 import KioskModal from "./kiosk-modal/Kiosk-Modal.vue";
+import kioskMixin from "../../mixins/kiosk/kioskMixin";
 
 export default {
   name: "Kiosk",
   components: { KioskModal },
-  mixins: [],
+  mixins: [kioskMixin],
   data() {
     return {
       discountEarned: 0,
@@ -60,32 +54,6 @@ export default {
       numberOfMatches: 0,
       numberOfTurns: 1,
       modalText: {},
-      cardDeck: [
-        {
-          cardName: "camaro1",
-          image: "/img/cars/gallery/chevrolet/sm/camaro_sm.jpg",
-        },
-        {
-          cardName: "camaro2",
-          image: "/img/cars/gallery/chevrolet/sm/camaro_sm.jpg",
-        },
-        {
-          cardName: "camaro3",
-          image: "/img/cars/gallery/chevrolet/sm/camaro_sm.jpg",
-        },
-        {
-          cardName: "camaro4",
-          image: "/img/cars/gallery/chevrolet/sm/camaro_sm.jpg",
-        },
-        {
-          cardName: "camaro5",
-          image: "/img/cars/gallery/chevrolet/sm/camaro_sm.jpg",
-        },
-        {
-          cardName: "camaro6",
-          image: "/img/cars/gallery/chevrolet/sm/camaro_sm.jpg",
-        },
-      ],
     };
   },
   created() {
@@ -100,42 +68,38 @@ export default {
       }
     },
     showModal(modalStatus) {
-      if (modalStatus === "start") {
-        this.isModalVisible = true;
-        this.modalText.header = "Welcome! Let's get started!";
-        this.modalText.body =
-          "You have 3 tries to find up to 3 matches. Each match found will earn you a larger discount.";
-        this.modalText.footer = "Tap the button below to get started.";
-      } else if (modalStatus === "success") {
-        this.isModalVisible = true;
-        this.modalText.header = "Nice job! You found a match!";
-        this.modalText.body = `You've found ${this.numberOfMatches} match${
-          this.numberOfMatches > 1 || this.numberOfMatches === 0 ? "es" : ""
-        } so far`;
-        this.modalText.footer = `You have ${
-          3 - this.numberOfTurns
-        } turns remaining`;
-      } else if (modalStatus === "failure") {
-        this.isModalVisible = true;
-        this.modalText.header = "So Close! You didn't find a match this time.";
-        this.modalText.body = `You've found ${this.numberOfMatches} match${
-          this.numberOfMatches > 1 || this.numberOfMatches === 0 ? "es" : ""
-        } so far`;
-        this.modalText.footer = `You have ${
-          3 - this.numberOfTurns
-        } turns remaining`;
-      } else if (modalStatus === "finished") {
-        this.isModalVisible = true;
-        this.modalText.header = `You've earned a ${this.discountEarned}% discount!`;
-        this.modalText.body = `Thanks for playing! You found ${
-          this.numberOfMatches
-        } match${
-          this.numberOfMatches > 1 || this.numberOfMatches === 0 ? "es" : ""
-        }!`;
-        this.modalText.footer = "";
-      } else {
-        this.isModalVisible = false;
-      }
+      setTimeout(() => {
+        if (modalStatus === "start") {
+          this.isModalVisible = true;
+          this.modalText.header = "Welcome! Let's get started!";
+          this.modalText.body =
+            "You have 3 tries to find up to 3 matches. Each match found will earn you a larger discount.";
+          this.modalText.footer = "Tap the button below to get started.";
+        } else if (modalStatus === "success") {
+          this.isModalVisible = true;
+          this.modalText.header = "Nice job! You found a match!";
+          this.modalText.body = `You've found ${this.numberOfMatches} match${
+            this.numberOfMatches > 1 || this.numberOfMatches === 0 ? "es" : ""
+          } so far`;
+          this.modalText.footer = `You have ${3 - this.numberOfTurns} turns remaining`;
+        } else if (modalStatus === "failure") {
+          this.isModalVisible = true;
+          this.modalText.header = "So Close! You didn't find a match this time.";
+          this.modalText.body = `You've found ${this.numberOfMatches} match${
+            this.numberOfMatches > 1 || this.numberOfMatches === 0 ? "es" : ""
+          } so far`;
+          this.modalText.footer = `You have ${3 - this.numberOfTurns} turns remaining`;
+        } else if (modalStatus === "finished") {
+          this.isModalVisible = true;
+          this.modalText.header = `You've earned a ${this.discountEarned}% discount!`;
+          this.modalText.body = `Thanks for playing! You found ${this.numberOfMatches} match${
+            this.numberOfMatches > 1 || this.numberOfMatches === 0 ? "es" : ""
+          }!`;
+          this.modalText.footer = "";
+        } else {
+          this.isModalVisible = false;
+        }
+      }, 800);
     },
     flipCard(card) {
       if (card.isMatched || card.isFlipped || this.flippedCards.length === 2) {
@@ -151,13 +115,7 @@ export default {
       }
     },
     match() {
-      console.log("Checking for card match");
       if (this.flippedCards[0].cardName === this.flippedCards[1].cardName) {
-        // setTimeout(() => {
-        //   this.flippedCards.forEach((card) => {
-        //     card.isMatched = true;
-        //   });
-        // }, 500);
         this.flippedCards.forEach((card) => {
           card.isMatched = true;
         });
@@ -165,32 +123,23 @@ export default {
         this.numberOfTurns++;
         this.flippedCards = [];
         this.showModal("success");
-        console.log("Success! Cards match", this.numberOfMatches);
-        //briefly show success modal with number of turns remaining
       } else {
         setTimeout(() => {
           this.flippedCards.forEach((card) => {
             card.isFlipped = false;
           });
         }, 800);
-        // this.flippedCards.forEach((card) => {
-        //   card.isFlipped = false;
-        // });
         this.numberOfTurns++;
         this.flippedCards = [];
         if (this.numberOfTurns < 3) {
           this.showModal("failure");
         }
-        console.log("Cards are not a match!", this.numberOfTurns);
-        //briefly show failure modal with number of turns remaining
-      }
 
-      if (this.numberOfTurns === 3) {
-        this.isGameFinished = true;
-        this.calculateDiscount(this.numberOfMatches);
-        this.showModal("finished");
-        // this.resetGame();
-        //show thanks for playing modal and redirect to form to caputure user info and pass along proper discount code
+        if (this.numberOfTurns === 3) {
+          this.isGameFinished = true;
+          this.calculateDiscount(this.numberOfMatches);
+          this.showModal("finished");
+        }
       }
     },
     resetGame() {
@@ -203,11 +152,8 @@ export default {
         this.memoryCards = [];
         const cards1 = _cloneDeep(this.cardDeck);
         const cards2 = _cloneDeep(this.cardDeck);
-        const cards3 = _cloneDeep(this.cardDeck);
 
-        this.memoryCards = _shuffle(
-          this.memoryCards.concat(cards1, cards2, cards3)
-        );
+        this.memoryCards = _shuffle(this.memoryCards.concat(cards1, cards2));
         this.isGameFinished = false;
         this.numberOfMatches = 0;
         this.numberOfTurns = 0;
---
+++ src/mixins/kiosk/kioskMixin.js
@@ -0,0 +1,44 @@
+export default {
+  data() {
+    return {
+      cardDeck: [
+        {
+          cardName: "camaro1",
+          image: "/img/cars/gallery/chevrolet/sm/camaro_sm.jpg",
+        },
+        {
+          cardName: "corvette1",
+          image: "/img/cars/gallery/chevrolet/sm/corvette_sm.jpg",
+        },
+        {
+          cardName: "tahoe1",
+          image: "/img/cars/gallery/chevrolet/sm/tahoe_sm.jpg",
+        },
+        {
+          cardName: "bmw_m4",
+          image: "/img/cars/gallery/bmw/sm/m4_sm.jpg",
+        },
+        {
+          cardName: "jeep1",
+          image: "/img/cars/gallery/jeep/sm/gladiator_2_sm.jpg",
+        },
+        {
+          cardName: "nissan1",
+          image: "/img/cars/gallery/nissan/370z_sm.jpg",
+        },
+        {
+          cardName: "gmc1",
+          image: "/img/cars/gallery/gmc_buick/sm/sierra_3_sm.jpg",
+        },
+        {
+          cardName: "hyundai1",
+          image: "/img/cars/gallery/hyundai/sm/genesis_sm.jpg",
+        },
+        {
+          cardName: "bmw1",
+          image: "/img/cars/gallery/bmw/sm/x3_sm.jpg",
+        },
+      ],
+    };
+  },
+};
