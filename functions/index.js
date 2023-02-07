41df33fa4a8d3fb35d27b267e3cfb267ac7be7a1
Chris Philbin <chrisp@runway.com.au>
2023-02-07 09:30:00 -0500

    Creating schedule function to test consultation form each day at 07:00EST

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const cors = require("cors");
 const app = require("express")();
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 app.use(cors());
 
@@ -13,3 +15,34 @@ app.post("/comment/new", submitComment);
 app.post("/kiosk", sendKioskResponse);
 
 exports.api = functions.https.onRequest(app);
+
+const testRequest = {
+  body: {
+    appointmentDetails: {
+      firstName: "Test",
+      lastName: "Test",
+      phone: "281-330-8004",
+      email: "test@test.com",
+      carYear: new Date().getFullYear().toString(),
+      carMake: "Chevy",
+      carModel: "Corvette",
+      carMilage: "1500",
+      servicesDesired: ["Ceramic Coating"],
+      otherComments: "Daily automated test of the consultation form.",
+      bestTimesToConnect: ["Between 9am and 12pm"],
+      campaignSource: "unknown",
+    },
+  },
+};
+
+const testResponse = {};
+
+exports.scheduledMailerTest = functions.pubsub
+  .schedule("30 07 * * *")
+  .timeZone("America/New_York")
+  .onRun(async (context) => {
+    const result = await sendAppointmentDetails(testRequest, testResponse);
+    console.log("Ran scheduled task!");
+    console.log(result);
+    return null;
+  });
