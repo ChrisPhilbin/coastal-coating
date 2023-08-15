9c56a15b3d59cf50eefb4853011614ffd20d6fc5
Chris Philbin <chrisp@runway.com.au>
2023-08-15 09:38:30 -0400

    implementing animation on hover

--- src/components/home/ServiceTiles.vue
+++ src/components/home/ServiceTiles.vue
@@ -1,29 +1,68 @@
+<style scoped>
+.flip-content {
+  transition: transform 0.4s;
+  transform-style: preserve-3d;
+}
+
+.flip-content:hover .flip-content {
+  transform: rotateY(180deg);
+  transition: transform 0.3s;
+}
+
+.flip-front,
+.flip-back {
+  backface-visibility: hidden;
+}
+
+.flip-back {
+  transform: rotateY(180deg);
+}
+</style>
+
 <template>
   <div class="grid gap-0 grid-cols-1 md:grid-cols-3">
     <div
-      class="bg-red-200 h-96 p-8 bg-center bg-no-repeat bg-cover"
+      class="h-96 p-8 bg-center bg-no-repeat bg-cover"
       style="background-image: url('/img/services/tiles/ceramic_coating.jpeg')"
     >
-      <div
-        class="flex items-center justify-center h-full cursor-pointer"
-        style="background-color: rgb(216, 180, 254, 0.4)"
-      >
-        <p class="text-xl text-white font-bold opacity-100">CERAMIC COATING</p>
+      <div class="flip-content h-full">
+        <div class="flip-front flex items-center bg-service-tile-front justify-center h-full cursor-pointer">
+          <div class="basis-full">
+            <p class="block text-2xl text-white font-bold opacity-100">CERAMIC COATING</p>
+          </div>
+        </div>
+        <div class="flip-back flex items-center bg-service-tile-back justify-center h-full cursor-pointer">
+          <div class="basis-full">
+            <p class="block text-2xl text-white font-bold opacity-100">CERAMIC COATING</p>
+            <button class="bg-gray-800 text-white p-3 mt-3">LEARN MORE</button>
+          </div>
+        </div>
       </div>
     </div>
 
     <div
-      class="bg-red-200 h-96 p-8 bg-center bg-no-repeat bg-cover"
+      class="h-96 p-8 bg-center bg-no-repeat bg-cover"
       style="background-image: url('/img/services/tiles/marine.jpg')"
     >
-      <div class="h-full bg-purple-300 opacity-40"></div>
+      <div
+        class="flex items-center bg-service-tile-front hover:bg-service-tile-back justify-center h-full cursor-pointer"
+      >
+        <div class="basis-full">
+          <p class="block text-2xl text-white font-bold opacity-100">MARINE</p>
+          <button class="bg-gray-800 text-white p-3 mt-3">LEARN MORE</button>
+        </div>
+      </div>
     </div>
 
-    <div
-      class="bg-red-200 h-96 p-8 bg-center bg-no-repeat bg-cover"
-      style="background-image: url('/img/services/tiles/rv.jpg')"
-    >
-      <div class="h-full bg-purple-300 opacity-40"></div>
+    <div class="h-96 p-8 bg-center bg-no-repeat bg-cover" style="background-image: url('/img/services/tiles/rv.jpg')">
+      <div
+        class="flex items-center bg-service-tile-front hover:bg-service-tile-back justify-center h-full cursor-pointer"
+      >
+        <div class="basis-full">
+          <p class="block text-2xl text-white font-bold opacity-100">RV</p>
+          <button class="bg-gray-800 text-white p-3 mt-3">LEARN MORE</button>
+        </div>
+      </div>
     </div>
 
     <div class="bg-green-200 h-min p-8">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,22 @@
+const plugin = require("tailwindcss/plugin");
+
+const customCardFlipClass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
@@ -13,6 +32,8 @@ module.exports = {
       colors: {
         "coastal-light-blue": "#59c2bb",
         "coastal-dark-blue": "#0d7ec0",
+        "service-tile-front": "rgb(216, 180, 254, 0.4)",
+        "service-tile-back": "rgb(22, 22, 22, 0.4)",
       },
       fontFamily: {
         "coastal-body": ["Nunito"],
@@ -27,5 +48,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [customCardFlipClass],
 };
